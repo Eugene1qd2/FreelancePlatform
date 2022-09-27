577e416a033d4966776dd245e4fb1cf3ea66e087
Eugene <qawsed200412345@gmail.com>
2022-09-27 23:10:44 +0300

    ModalWindow.xaml uploaded RoundedButtonTheme.xaml uploaded

---
+++ ModalWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FreelancePlatform
+{
+    /// <summary>
+    /// Interaction logic for ModalWindow.xaml
+    /// </summary>
+    public partial class ModalWindow : Window
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        public ModalWindow(string text)
+        {
+            Text = text;
+            InitializeComponent();
+        }
+
+        private void CloseButtonClick(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
