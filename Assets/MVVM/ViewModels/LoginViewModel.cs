2a6edb8e655b200db41bdb94521c3efc450abd9e
Eugene <qawsed200412345@gmail.com>
2022-10-02 01:46:09 +0300

    Remade EnterWindow. Started MVVM structure. Dear diary, I cannot put into words the pain of disappointment in WPF technology and MVVM structure. I've never been so wrong. I sincerely repent. I have already redone all the styles for the umpteenth time, just so that the login form works. May Microsoft bless me for completing this fucking project.

---
+++ Assets/MVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FreelancePlatform.Assets.MVVM.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged();
+            }
+
+        }
+        public SecureString Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+
+        }
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; OnPropertyChanged(); }
+
+        }
+        public bool IsViewVisible
+        {
+            get { return _isViewVisible; }
+            set { _isViewVisible = value; OnPropertyChanged(); }
+
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RememberPasswordCommand = new ViewModelCommand(p=>ExecuteRememberPasswordCommand("",""));
+        }
+
+        private bool ExecuteRememberPasswordCommand(string username , string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length<3)
+            {
+                validData = false;
+
+            }
+            else
+            {
+                validData = true;
+            }
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
